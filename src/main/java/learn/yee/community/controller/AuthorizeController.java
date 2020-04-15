e1f04131e72255847538884f92d8a3e23de344ef
yee-hui <doris0163@163.com>
2020-04-15 22:13:27 +0800

    修改配置

--- src/main/java/learn/yee/community/controller/AuthorizeController.java
+++ src/main/java/learn/yee/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import learn.yee.community.dto.AccessTokenDTO;
 import learn.yee.community.dto.GithubUser;
 import learn.yee.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private  String clientId;
+    @Value("${github.client.secret}")
+    private  String secret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("6139c8b77d09575305c3");
-        accessTokenDTO.setClient_secret("94e47ebe80fb99fe4041f68c3837cdc16a1396de");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
